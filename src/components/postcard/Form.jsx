bc7ec32e1f6f8e9e8f74d44a142a8ad9faba1503
Victor Blanco <12vblanco@gmail.com>
2023-03-26 22:17:33 +0100

    hideSocial

--- src/components/postcard/Form.jsx
+++ src/components/postcard/Form.jsx
@@ -9,13 +9,7 @@ const From = () => {
         <Text>Contact 'Postcard' Form</Text>
         <Stamp>No Stamp Required</Stamp>
       </Row>
-      <form
-        name="contact"
-        netlify
-        data-netlify="true"
-        method="POST"
-        netlify-honeypot="bot-field"
-      >
+      <form name="contact" netlify method="POST" netlify-honeypot="bot-field">
         <input type="hidden" name="bot-field" />
         <Row>
           <Message>
