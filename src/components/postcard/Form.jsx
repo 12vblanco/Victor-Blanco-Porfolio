3f8a2cebb2dbbc94545056024d2c0cc0826249df
Victor Blanco <12vblanco@gmail.com>
2023-03-27 17:35:13 +0100

    homeFix

--- src/components/postcard/Form.jsx
+++ src/components/postcard/Form.jsx
@@ -10,14 +10,7 @@ const From = () => {
         <Stamp>No Stamp Required</Stamp>
       </Row>
 
-      <form
-        name="contact"
-        method="post"
-        data-netlify-honeypot="bot-field"
-        data-netlify-recaptcha="true"
-        onSubmit="submit"
-        action="/Success/"
-      >
+      <form name="contact" method="post" onSubmit="submit" action="/Success/">
         <input type="hidden" name="form-name" value="contact" />
         <input type="hidden" name="bot-field" />
         <Row>
