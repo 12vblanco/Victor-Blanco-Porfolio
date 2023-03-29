f852c0f79106b220f2b57756544b24ff6d587559
Victor Blanco <12vblanco@gmail.com>
2023-03-29 17:52:05 +0100

    cookieBanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import CookieConsent from "react-cookie-consent";
+import styled from "styled-components";
 import "./App.css";
 import Footer from "./components/footer/Footer";
 import HorizontalNav from "./components/navigation/HorizontalNav";
 import VerticalNav from "./components/navigation/VerticalNav";
 import Home from "./components/pages/Home";
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -15,6 +18,31 @@ function App() {
 
   return (
     <>
+      <CookieConsent
+        style={{
+          background: "rgba(2, 60, 113, 0.8)",
+          fontWeight: "500",
+          height: "10vh",
+          alignItems: "center",
+          fontSize: "18px",
+        }}
+        buttonStyle={{
+          borderRadius: "50px",
+          padding: "20px 32px",
+          fontSize: "18px",
+          fontWeight: "700",
+          background: "white",
+          color: "rgb(2, 60, 113)",
+        }}
+        contentStyle={{ textAlign: "left" }}
+      >
+        This website uses cookies and third party software to monitor traffic
+        anonymously and improve user experience. For more info read my{" "}
+        <Span>
+          <a href="/terms">terms & conditions</a>
+        </Span>
+        .
+      </CookieConsent>
       <HorizontalNav handleToggle={handleToggle} isOpen={isOpen} />
       <VerticalNav handleToggle={handleToggle} isOpen={isOpen} />
       <Home />
@@ -22,5 +50,12 @@ function App() {
     </>
   );
 }
+
+const Span = styled.span`
+  a {
+    color: white;
+    text-decoration: underline;
+  }
+`;
 
 export default App;
---
+++ src/terms.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Terms = () => {
+  return (
+    <>
+      <h1>Terms & Conditions</h1>
+      <p>
+        bla bla bla la blab blbla bla bla la blab blbla bla bla la blab blbla
+        bla bla la blab blbla bla bla la blab blbla bla bla la blab blbla bla
+        bla la blab bla
+      </p>
+    </>
+  );
+};
+
+export default Terms;
