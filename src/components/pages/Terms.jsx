1846387a1a3719e44a7792133a039b21909d03a1
Victor Blanco <12vblanco@gmail.com>
2023-03-30 17:41:48 +0100

    scrollProblem

---
+++ src/components/pages/Terms.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+const Terms = () => {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>
+        Welcome to Victor Blanco Web. Before using our website, please read the
+        following terms and conditions carefully. By using our website, you
+        agree to be bound by these terms and conditions.
+      </p>
+      <ol>
+        <li>
+          Use of Cookies:
+          <p>
+            We use cookies on our website to provide you with the best possible
+            user experience. By using our website, you agree to the use of
+            cookies. We do not collect any personal information through our
+            cookies.
+          </p>
+        </li>
+        <li>
+          Use of Google Analytics:
+          <p>
+            We use Google Analytics on our website to collect information about
+            how visitors use our website. This information is used to improve
+            our website and to provide better services to our visitors. Google
+            Analytics collects information anonymously and reports website
+            trends without identifying individual visitors.
+          </p>
+        </li>
+        <li>
+          Contact Form:
+          <p>
+            We provide a contact form on our website to allow visitors to
+            contact us. Any information you provide on the contact form is used
+            solely to respond to your inquiry. We do not share your information
+            with any third party.
+          </p>
+        </li>
+        <li>
+          Intellectual Property:
+          <p>
+            All content on our website, including text, images, and logos, is
+            the property of Victor Blanco Web and is protected by international
+            copyright laws. You may not use any content from our website without
+            our prior written permission.
+          </p>
+        </li>
+        <li>
+          Links to Other Websites:
+          <p>
+            Our website may contain links to other websites. We are not
+            responsible for the content or privacy policies of those websites.
+            We encourage you to read the terms and conditions and privacy
+            policies of those websites before using them.
+          </p>
+        </li>
+        <li>
+          Limitation of Liability:
+          <p>
+            Victor Blanco Web is not responsible for any damages, including but
+            not limited to, direct or indirect damages, arising from the use of
+            our website or the information provided on our website.
+          </p>
+        </li>
+        <li>
+          Applicable Law:
+          <p>
+            These terms and conditions shall be governed by and construed in
+            accordance with the laws of the United Kingdom and the European
+            Union.
+          </p>
+        </li>
+        <li>
+          Amendments:
+          <p>
+            We reserve the right to modify these terms and conditions at any
+            time without prior notice. Your continued use of our website after
+            any changes to these terms and conditions constitutes your
+            acceptance of the new terms and conditions.
+          </p>
+        </li>
+      </ol>
+      <p>
+        If you have any questions or comments regarding these terms and
+        conditions, please contact us using the contact form on our website.
+      </p>
+    </div>
+  );
+};
+
+export default Terms;
