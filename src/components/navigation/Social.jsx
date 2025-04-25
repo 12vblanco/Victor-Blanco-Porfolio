9dd0e59d613815ad6746e43cee627cde184f8015
Victor Blanco <12vblanco@gmail.com>
2025-04-25 15:41:49 +0100

    removed linkedin

--- src/components/navigation/Social.jsx
+++ src/components/navigation/Social.jsx
@@ -31,16 +31,16 @@ const Social = ({ flexDirection, isOpen }) => {
               <GrInstagram />
             </a>
           </Icon>
-          <Icon>
-            {/* <a
+          {/* <Icon>
+            <a
               href="https://www.linkedin.com/in/12vblanco/"
               target="_blank"
               rel="noreferrer"
               aria-label="Visit Victor Blanco's Linkedin Page"
             >
               <GrLinkedin />{" "}
-            </a> */}
-          </Icon>
+            </a>
+          </Icon> */}
           <Icon>
             <a
               href="https://www.github.com/12vblanco/"
